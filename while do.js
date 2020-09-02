c2288276694257c84990380f682ef189b5af14a9
Parth Korawala <50219075+parthk3004@users.noreply.github.com>
2020-09-02 09:40:56 +0530

    Add files via upload

---
+++ while do.js
@@ -0,0 +1,18 @@
+var aList=[
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    12345
+];
+let index=0;
+while (index<aList.length) {
+    console.log(aList[index]);
+    index+=1;
+}
+let i=1;
+do{
+    console.log(i**2);
+    i++;
+}while(i<=10)
